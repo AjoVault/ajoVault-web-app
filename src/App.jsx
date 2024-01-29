95896061de85f20bef654a816b365eef172d9263
Elizabeth Awe <alizzyshire@gmail.com>
2024-01-29 21:41:43 +0100

    Latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+import Register from './components/Register/Register.jsx'
+import Login from './components/Login/Login.jsx'
+import Modal from './components/Modal/modal.jsx'
+// import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import RegisterOutlet from './RegisterLayout.jsx'
+import OTPModal from './components/Modal/OTPModal.jsx'
+import EnterOTP from './components/Modal/EnterOTP.jsx'
+import Password from './components/Modal/Password.jsx'
+import PinModal from './components/Modal/PinModal.jsx'
+import ConfirmPin from './components/Modal/ConfirmPin.jsx'
+
 
 function App() {
     return (
@@ -8,6 +23,16 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path='/register/' element={<RegisterOutlet />}>
+                    <Route path='checkemail' element={<Modal />}  />
+                    <Route path='otp' element={<OTPModal />} />
+                    <Route path='inputotp' element={<EnterOTP />} />
+                    <Route path='password' element={<Password />} />
+                    <Route path='pin' element={<PinModal />} />
+                    <Route path='confirmpin' element={<ConfirmPin />} />
+                    {/* </Route> */}   
+                </Route>
+                <Route path='/login' element={<Login />} />
             </Routes>
         </div>
     );
