991185374e27d986e3b396f29c1fe135e1479aa3
Oladele Balogun <bandechop01@gmail.com>
2024-01-31 19:25:58 +0100

    feat: carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components-landingPage/navbar/navbar";
 import Hero from "./components-landingPage/hero/hero";
 import ProductF from "./components-landingPage/product-features/productF";
+import Success from "./components-landingPage/how-it-works/success";
+import Carousel from "./components-landingPage/Carousel/Carousel";
 
 
 function App() {
@@ -11,9 +13,14 @@ function App() {
         <Navbar/>
         <Hero/>
         <ProductF/>
+        <Success/>
+        <Carousel/>
       </div>
     </>
   );
 }
 
+// const data =[
+
+// ]
 export default App;
