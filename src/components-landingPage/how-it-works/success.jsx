6c573bb0e268d13030a569e338be667b29aa841a
Oladele Balogun <bandechop01@gmail.com>
2024-02-03 12:11:02 +0100

    feat: footer

--- src/components-landingPage/how-it-works/success.jsx
+++ src/components-landingPage/how-it-works/success.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import "./success.css"
-const success = () => {
+import React from 'react';
+import "./success.css";
+import Carousel from "../Carousel/Carousel";
+
+const Success = () => {
   return (
     <div>
       <section className="testimonials">
         <div className="testimonials-container">
-            <h1>How it Works</h1>
-            <div className="list-container1">
-                <ul>
-                    <li>Get started by signing Up or Logging in</li>
-                    <li>Choose one or more savings palns</li>
-                    <li>Complete KYC and sign standing order</li>
-                    <li>Savings payments and withdrawals are Automated</li>
-                </ul>
-                <div className="list-container2"></div>
-            </div>
+        <div className='testimonial-header'><h1>How it Works</h1>
+          <h1>Success Stories</h1>
+          </div>
+          <div className="secondary-container">
+          <div className="list-container1">
+            <ul>
+              <li>Get started by signing Up or Logging in</li>
+              <li>Choose one or more savings plans</li>
+              <li>Complete KYC and sign standing order</li>
+              <li>Savings payments and withdrawals are Automated</li>
+            </ul>
+            <div className="list-container2"></div>
+          </div>
+          <div className='carousel-container'>
+            <Carousel />
+          </div>
+          </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default success
+export default Success;
