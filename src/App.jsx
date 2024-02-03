224c6e0ac36ca70247e588c0878e6fb9dca6428b
Oddisy <odunayoogunleye200@gmail.com>
2024-02-03 14:58:04 +0100

    fix: make dashboard responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,42 @@ import Hero from "./components-landingPage/hero/hero";
 import ProductF from "./components-landingPage/product-features/productF";
 import Success from "./components-landingPage/how-it-works/success";
 import Carousel from "./components-landingPage/Carousel/Carousel";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import Register from './components/Register/Register.jsx'
-import Login from './components/Login/Login.jsx'
-import Modal from './components/Modal/modal.jsx'
-import RegisterOutlet from './RegisterLayout.jsx'
-import OTPModal from './components/Modal/OTPModal.jsx'
-import EnterOTP from './components/Modal/EnterOTP.jsx'
-import Password from './components/Modal/Password.jsx'
-import PinModal from './components/Modal/PinModal.jsx'
-import ConfirmPin from './components/Modal/ConfirmPin.jsx'
+import Register from "./components/Register/Register.jsx";
+import Login from "./components/Login/Login.jsx";
+import Modal from "./components/Modal/modal.jsx";
+import RegisterOutlet from "./RegisterLayout.jsx";
+import OTPModal from "./components/Modal/OTPModal.jsx";
+import EnterOTP from "./components/Modal/EnterOTP.jsx";
+import Password from "./components/Modal/Password.jsx";
+import PinModal from "./components/Modal/PinModal.jsx";
+import ConfirmPin from "./components/Modal/ConfirmPin.jsx";
 
 function App() {
-    return (
-        <div>
-            <Navbar />
+	return (
+		<div>
+			{/* <Navbar />
             <Hero />
             <ProductF />
             <Success />
-            <Carousel />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path='/register/' element={<RegisterOutlet />}>
-                    <Route path='checkemail' element={<Modal />} />
-                    <Route path='otp' element={<OTPModal />} />
-                    <Route path='inputotp' element={<EnterOTP />} />
-                    <Route path='password' element={<Password />} />
-                    <Route path='pin' element={<PinModal />} />
-                    <Route path='confirmpin' element={<ConfirmPin />} />
-                </Route>
-                <Route path='/login' element={<Login />} />
-            </Routes>
-        </div>
-    );
+            <Carousel /> */}
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/register/" element={<RegisterOutlet />}>
+					<Route path="checkemail" element={<Modal />} />
+					<Route path="otp" element={<OTPModal />} />
+					<Route path="inputotp" element={<EnterOTP />} />
+					<Route path="password" element={<Password />} />
+					<Route path="pin" element={<PinModal />} />
+					<Route path="confirmpin" element={<ConfirmPin />} />
+				</Route>
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/components-landingPage/DashHome.jsx
+++ src/components-landingPage/DashHome.jsx
@@ -3,106 +3,95 @@ import progressbar from "../assets/progressbar.svg";
 import circle from "../assets/circle.svg";
 
 const DashHome = () => {
-    return (
-        <section className="flex flex-col pr-4 pt-10 pb-4 lg:w-[90%] lg:mx-auto">
-            <div className="flex gap-3 justify-end items-center">
-                <DashLoginProfile />
-            </div>
-            <div className="pt-4 pb-6">
-                <h1 className="text-[40px] font-semibold">Isaac</h1>
-                <p className="text-[#808080] text-lg text-nowrap">
-                    Hello, let&apos;s build today!
-                </p>
-            </div>
-            <section className="flex gap-6 lg:justify-between items-center flex-row">
-                <div className="bg-[#E2DDFE] shrink-0 text-center px-4 py-8 rounded-lg md:w-1/2">
-                    <h1 className="font-medium text-xl">
-                        Complete your KYC to set up your account
-                    </h1>
-                    <img
-                        src={progressbar}
-                        alt="progress bar"
-                        className="py-6"
-                    />
-                    <button className="text-white bg-[#5A47CF] text-center rounded-lg py-3 px-8">
-                        Continue
-                    </button>
-                </div>
+	return (
+		<section className="flex flex-col pr-4 pt-10 pb-4 pb-16 lg:w-[90%] lg:mx-auto">
+			<div className="flex gap-3 justify-end items-center">
+				<DashLoginProfile />
+			</div>
+			<div className="pt-4 pb-6">
+				<h1 className="text-[40px] font-semibold">Isaac</h1>
+				<p className="text-[#808080] text-lg text-nowrap">
+					Hello, let&apos;s build today!
+				</p>
+			</div>
+			<section className="flex gap-6 lg:justify-between items-center flex-col lg:flex-row">
+				<div className="bg-[#E2DDFE] shrink-0 text-center px-4 py-8 rounded-lg max-md:w-full lg:w-1/2">
+					<h1 className="font-medium text-xl">
+						Complete your KYC to set up your account
+					</h1>
+					<img src={progressbar} alt="progress bar" className="py-6" />
+					<button className="text-white bg-[#5A47CF] text-center rounded-lg py-3 px-8">
+						Continue
+					</button>
+				</div>
 
-                <div className="shrink-0 mb-12">
-                    <h1 className="text-[33px] font-semibold">To do list</h1>
-                    <div className="bg-white flex flex-col gap-3 rounded-lg card0 px-2 py-4">
-                        <div className="flex gap-4 items-center">
-                            <img src={circle} alt="circle" className="w-4" />
-                            <h3 className="font-normal text-[23px]">
-                                Complete KYC
-                            </h3>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <img src={circle} alt="circle" className="w-4" />
-                            <h3 className="font-normal text-[23px]">
-                                Link bank for a direct debit
-                            </h3>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <img src={circle} alt="circle" className="w-4" />
-                            <h3 className="font-normal text-[23px]">
-                                Join a savings plan
-                            </h3>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <img src={circle} alt="circle" className="w-4" />
-                            <h3 className="font-normal text-[23px]">
-                                Invite friends to earn bonus
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </section>
+				<div className="shrink-0 mb-12">
+					<h1 className="text-[33px] font-semibold">To do list</h1>
+					<div className="bg-white flex flex-col gap-3 rounded-lg card0 px-2 py-4">
+						<div className="flex gap-4 items-center">
+							<img src={circle} alt="circle" className="w-4" />
+							<h3 className="font-normal text-[23px]">Complete KYC</h3>
+						</div>
+						<div className="flex gap-4 items-center">
+							<img src={circle} alt="circle" className="w-4" />
+							<h3 className="font-normal text-[23px]">
+								Link bank for a direct debit
+							</h3>
+						</div>
+						<div className="flex gap-4 items-center">
+							<img src={circle} alt="circle" className="w-4" />
+							<h3 className="font-normal text-[23px]">Join a savings plan</h3>
+						</div>
+						<div className="flex gap-4 items-center">
+							<img src={circle} alt="circle" className="w-4" />
+							<h3 className="font-normal text-[23px]">
+								Invite friends to earn bonus
+							</h3>
+						</div>
+					</div>
+				</div>
+			</section>
 
-            <section className="flex flex-col">
-                <h1 className="text-[33px] font-semibold text-nowrap">
-                    Quick Access
-                </h1>
-                <section className="pt-10 flex gap-10 lg:justify-between">
-                    <div className="flex flex-col gap-4 shrink-0 bg-[#5A47CF] text-white card1 py-6 px-10">
-                        <h3 className="text-2xl">
-                            Pooled <br /> Contribution
-                        </h3>
-                        <p className="max-w-44 pb-4">
-                            Join a batch savings pool based on your budget
-                        </p>
-                        <button className="border-2 border-[#5E0035] py-3 rounded-lg">
-                            Join Now
-                        </button>
-                    </div>
-                    <div className="flex flex-col gap-3 shrink-0 bg-[#5E0035] card2 text-white py-6 px-10">
-                        <h3 className="text-2xl">
-                            Personal <br /> Savings
-                        </h3>
-                        <p className="max-w-[11rem] pb-4">
-                            Start a personal savings to meet your goal
-                        </p>
-                        <button className="border-2 border-[#5A47CF] py-3 rounded-lg">
-                            Start Now
-                        </button>
-                    </div>
-                    <div className="flex flex-col gap-3 shrink-0 bg-[#B5AAFC] card3 py-6 px-10">
-                        <h3 className="text-2xl">
-                            Creating a <br /> Savings Pool
-                        </h3>
-                        <p className="max-w-[12rem] pb-4">
-                            Invite friends to create a pooled contribution that
-                            works for all
-                        </p>
-                        <button className="border-2 border-[#5A47CF] rounded-lg py-3">
-                            Create Now
-                        </button>
-                    </div>
-                </section>
-            </section>
-        </section>
-    );
+			<section className="flex flex-col">
+				<h1 className="text-[33px] font-semibold text-nowrap">Quick Access</h1>
+				<section className="pt-10 flex gap-10 lg:justify-between flex-col">
+					<div className="flex flex-col gap-4 shrink-0 bg-[#5A47CF] text-white card1 py-6 px-10">
+						<h3 className="text-2xl">
+							Pooled <br /> Contribution
+						</h3>
+						<p className="max-w-44 pb-4">
+							Join a batch savings pool based on your budget
+						</p>
+						<button className="border-2 border-[#5E0035] py-3 rounded-lg">
+							Join Now
+						</button>
+					</div>
+					<div className="flex flex-col gap-3 shrink-0 bg-[#5E0035] card2 text-white py-6 px-10">
+						<h3 className="text-2xl">
+							Personal <br /> Savings
+						</h3>
+						<p className="max-w-[11rem] pb-4">
+							Start a personal savings to meet your goal
+						</p>
+						<button className="border-2 border-[#5A47CF] py-3 rounded-lg">
+							Start Now
+						</button>
+					</div>
+					<div className="flex flex-col gap-3 shrink-0 bg-[#B5AAFC] card3 py-6 px-10">
+						<h3 className="text-2xl">
+							Creating a <br /> Savings Pool
+						</h3>
+						<p className="max-w-[12rem] pb-4">
+							Invite friends to create a pooled contribution that works for all
+						</p>
+						<button className="border-2 border-[#5A47CF] rounded-lg py-3">
+							Create Now
+						</button>
+					</div>
+				</section>
+			</section>
+		</section>
+	);
 };
 
 export default DashHome;
