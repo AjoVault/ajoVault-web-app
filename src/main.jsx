c210e4f1f2237f0d2ccef8b08f6e62dfcece08a1
Elizabeth Awe <alizzyshire@gmail.com>
2024-02-08 22:11:52 +0100

    Spinner

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import UserContextProvider from "./context/UserContextProvider.jsx";
-import Spinner from "./components/spinner/spinner.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
