4176438dd0251c8b20b99fc46d50392f55814e03
AjoVault <157455930+AjoVault@users.noreply.github.com>
2024-02-19 08:42:01 +0100

    Delete node-express-server directory from the dashboard branch

--- node-express-server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController')
-
-// Login User
-router.post('/login', authController.loginUser );
-
-module.exports = router;
--- node-express-server/routes/utilRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const utilController = require('../controllers/utilController')
-
-// Login User
-router.post('/login', authController.loginUser );
-
-module.exports = router;
